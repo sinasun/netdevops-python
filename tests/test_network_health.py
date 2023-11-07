630849558a05b37978a89ae10fb29fd1164d9363
sinasun <sinakhodaveisi@gmail.com>
2023-11-06 23:36:56 -0500

    Added: unit testing for network_health functions

---
+++ tests/test_network_health.py
@@ -0,0 +1,24 @@
+from app.network_health import ping, check_port, speed_test
+
+def test_ping_success():
+    host = "google.com"
+    assert ping(host) is True
+
+def test_ping_failure():
+    host = "non-exist-host.com"
+    assert ping(host) is False
+
+def test_check_open_port():
+    host = "google.com"
+    port = 80
+    assert check_port(host, port) is True
+
+def test_check_closed_port():
+    host = "google.com"
+    port = 8080
+    assert check_port(host, port) is False
+
+def test_speed_test():
+    download_speed, upload_speed = speed_test()
+    assert download_speed > 0
+    assert upload_speed > 0
