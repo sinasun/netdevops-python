41c84a2a4d3b9fca9554d64946626620b2c131d6
sinasun <sinakhodaveisi@gmail.com>
2023-11-07 18:57:26 -0500

    fixed: tests giving error when server is not connected to network

--- tests/test_network_health.py
+++ tests/test_network_health.py
@@ -1,42 +1,64 @@
 from app.network_health import ping, check_port, speed_test, NetworkNotConnectedException
-import pytest
 
 def test_ping_success():
     host = "google.com"
-    network_connected = False 
+    network_connected = True
     result = False
     try:
         result = ping(host)
-        network_connected = True
     except NetworkNotConnectedException:
         network_connected = False
+
     if network_connected is True:
         assert result is True
 
 def test_ping_failure():
     host = "non-exist-host.com"
-    with pytest.raises(NetworkNotConnectedException):
-        ping(host)
+    network_connected = True
+    result = True
+    try:
+        result = ping(host)
+    except NetworkNotConnectedException:
+        network_connected = False
+
+    if network_connected is True:
+        assert result is False
 
 def test_check_open_port():
     host = "google.com"
     port = 80
+    result = False
+    network_connected = True
     try:
         result = check_port(host, port)
-        assert result is True
     except NetworkNotConnectedException:
-        pass
+        network_connected = False
+    
+    if network_connected is True:
+        assert result is True
 
 def test_check_closed_port():
     host = "google.com"
     port = 8080
-    with pytest.raises(NetworkNotConnectedException):
-        check_port(host, port)
+    network_connected = True
+    result = True
+    try:
+        result = check_port(host, port)
+    except NetworkNotConnectedException:
+        network_connected = False
+
+    if network_connected is True:
+        assert result is False
 
 def test_speed_test():
+    download_speed = 0
+    upload_speed = 0
+    network_connected = True;
     try:
         download_speed, upload_speed = speed_test()
+    except NetworkNotConnectedException:
+        network_connected = False
+
+    if network_connected is True:
         assert download_speed > 0
         assert upload_speed > 0
-    except NetworkNotConnectedException:
-        pass
